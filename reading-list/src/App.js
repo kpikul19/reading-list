b4987601c63da7c20a8a8b282b00f138dcdf6a26
kp <kaitlinpikul@gmail.com>
2024-01-12 20:37:50 -0500

    more work on the reading list

--- reading-list/src/App.js
+++ reading-list/src/App.js
@@ -1,20 +1,48 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList"
 
 function App() {
     const [books, setBooks] = useState([]);
 
-    const createBook = (title) => {
-        const updatedBooks = [
-            
-        ]
+    const editBookByID = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+                return { ...book, title: newTitle };
+            } 
+
+            return book;
+        })
+
         setBooks(updatedBooks);
     };
 
-    return <div>
-        <BookCreate onCreate={createBook} /> 
-        </div>;
+
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id; //if book doesn't have the id of the book trying to be removed then it won't be removed
+        });
+
+        setBooks(updatedBooks);
+    };
+
+    const createBook = (title) => {
+        const updatedBooks = [
+            ...books, //copy all the books
+            { 
+                id: Math.round(Math.random() * 999), 
+                title 
+            } // insert new book with random id (ids are usually taken care of by a backend server but we don't have that) and the new title (title is equivalent to title:title because they are the same name)
+        ]
+        setBooks(updatedBooks); //new array with new book added 
+    };
+
+    return (
+        <div className="app">
+            <BookList books={books} onDelete={deleteBookById} onEdit={editBookByID} />
+            <BookCreate onCreate={createBook} /> 
+        </div>
+    );
 }
         //onCreate is a prop that is made here to take the createBook event handler
-
 export default App;
--- reading-list/src/components/BookShow.js
+++ reading-list/src/components/BookShow.js
@@ -1,5 +1,35 @@
-function BookShow () {
-    return <div>BookShow</div>;
+import { useState } from "react";
+import BookEdit from './BookEdit';
+
+function BookShow ( {book, onDelete, onEdit } ) {
+    const [showEdit, setShowEdit] = useState(false); //make edit form false so it won't show by default
+
+    const handleDeleteClick = () => {
+        onDelete(book.id);
+    };
+
+    const handleEditClick = () => {
+        setShowEdit(!showEdit); //toggle show edit on click
+    };
+
+    let content = <h3>{book.title}</h3>; //let allows you to change the variable over time, so this is saying by default we will show the book title
+    if (showEdit) { //so if showEdit is true, then display book edit instead of the book title
+        content = <BookEdit onEdit={onEdit} book={book} />; //passing down book object so that BookEdit receives it and can use the book title as the default value of the edit before a change is made to the title
+    }
+
+    return (
+        <div className="book-show">
+            <div>{content}</div>
+            <div className="actions">
+                <button className="edit" onClick={handleEditClick}>
+                    Edit
+                </button>
+                <button className="delete" onClick={handleDeleteClick}>
+                    Delete
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default BookShow;
