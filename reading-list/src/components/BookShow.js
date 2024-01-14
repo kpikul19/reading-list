3890b7b9dc5d02c3c9787bf7d7b3b4458fcb3fc2
Kaitlin Pikul <kaitlin@pair.com>
2024-01-13 22:29:05 -0600

    more updates to reading list

--- reading-list/src/components/BookShow.js
+++ reading-list/src/components/BookShow.js
@@ -12,13 +12,22 @@ function BookShow ( {book, onDelete, onEdit } ) {
         setShowEdit(!showEdit); //toggle show edit on click
     };
 
+    const handleSubmit = (id, newTitle) => {
+        setShowEdit(false);
+        onEdit(id, newTitle);
+    };
+
     let content = <h3>{book.title}</h3>; //let allows you to change the variable over time, so this is saying by default we will show the book title
     if (showEdit) { //so if showEdit is true, then display book edit instead of the book title
-        content = <BookEdit onEdit={onEdit} book={book} />; //passing down book object so that BookEdit receives it and can use the book title as the default value of the edit before a change is made to the title
+        content = <BookEdit onSubmit={handleSubmit} book={book} />; //passing down book object so that BookEdit receives it and can use the book title as the default value of the edit before a change is made to the title
     }
 
     return (
         <div className="book-show">
+            <img
+                alt="books"
+                src={`https://picsum.photos/seed/${book.id}/300/200`}
+            />
             <div>{content}</div>
             <div className="actions">
                 <button className="edit" onClick={handleEditClick}>
